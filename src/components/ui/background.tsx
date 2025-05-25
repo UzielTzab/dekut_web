8d1f173fca86ca57412b1d8ac85bf9e480c5970d
Uziel Tzab <uzieltzab8@gmail.com>
2025-05-25 12:27:47 -0600

    Se cambió la imagen de fondo del la página

--- src/components/ui/background.tsx
+++ src/components/ui/background.tsx
@@ -1,7 +1,7 @@
 export default function Background() {
   return (
     <div className="fixed inset-0 z-0 bg-[url('/images/background_dk.png')] bg-cover bg-center">
-      <div className="absolute inset-0 bg-gradient-to-b from-indigo-900/0 via-gray-900/95 to-black"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-indigo-900/10 via-gray-900/0 to-black"></div>
     </div>
   );
 }
